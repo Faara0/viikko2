f50db85ad1301d88ca3a754e22a67929da09c73c
Faara0 <toivovanhasaari@gmail.com>
2024-01-22 17:45:30 +0200

    siili

--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -1,37 +1,40 @@
 package main;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 public class App 
 {
     public static void main( String[] args) {
-        System.out.println("1) Pistä siili puhumaan, 2) Luo uusi siili, 3) Juoksuta siiliä, 0) Lopeta ohjelma");
-            System.out.print("Anna numero: ");
-            Scanner sc = new Scanner(System.in);
-            if(sc.hasNext()){
-                int i = 0; //user gives input which action they want
-                String input = sc.nextLine();
-                i = Integer.parseInt(input);
-            switch(i) {
-                    case 1:
+        int i = 0; //user gives input which action they want
+        do {
+            System.out.println("1) Pistä siili puhumaan, 2) Luo uusi siili, 3) Juoksuta siiliä, 0) Lopeta ohjelma");
+                Scanner bufferedScanner = new Scanner(System.in);
+                    String input = bufferedScanner.nextLine();
+                    i = Integer.parseInt(input);
+                    if(i == 1) {
                         System.out.println("Mitä siili sanoo: ");
-                        String speak = sc.nextLine();
+                        Scanner SpeakScanner = new Scanner(System.in);
+                        String speak = SpeakScanner.nextLine();
                         Hedgehog siili = new Hedgehog();
                         String speaking = siili.speak(speak);
                         System.out.println(speaking);
-                        break;
-                    case 2:
+                    }
+                    else if(i == 2) {
                         System.out.println("Valinta 2 toimii.");
-                        break;
-                    case 3:
+                        //break;
+                    }
+                    else if(i == 3) {
                         System.out.println("Valinta 3 toimii.");
-                        break;
-                    case 0:
+                        //break;
+                    }
+                    else if(i == 0) {
                         System.out.println("Kiitos ohjelman käytöstä.");
                         break;
-                    default:
+                    }
+                    else {
                         System.out.println("Syöte oli väärä");
-                }
-            }
-    sc.close();    
+                    }    
+        } while(i > 0);     
     }
 }
