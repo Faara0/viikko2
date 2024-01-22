a25a320d20c3330f57737b53c7c4daa1911bf284
Faara0 <toivovanhasaari@gmail.com>
2024-01-22 17:19:15 +0200

    siili

--- src/main/java/viikko2tehtavat/App.java
+++ src/main/java/viikko2tehtavat/App.java
@@ -1,9 +1,35 @@
 package viikko2tehtavat;
 
+import java.util.Scanner;
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Testiä toiselle viikolle." );
+    public static void main( String[] args) {
+        System.out.println("1) Pistä siili puhumaan, 2) Luo uusi siili, 3) Juoksuta siiliä, 0) Lopeta ohjelma");
+            System.out.print("Anna numero: ");
+            Scanner sc = new Scanner(System.in);
+            if(sc.hasNext()){
+                int i = 0; //user gives input which action they want
+                String input = sc.nextLine();
+                i = Integer.parseInt(input);
+            switch(i) {
+                    case 1:
+                        System.out.println("Mitä siili sanoo: ");
+                        String speak = sc.nextLine();
+                        System.out.println("Pikseli: " + speak );
+                        break;
+                    case 2:
+                        System.out.println("Valinta 2 toimii.");
+                        break;
+                    case 3:
+                        System.out.println("Valinta 3 toimii.");
+                        break;
+                    case 0:
+                        System.out.println("Kiitos ohjelman käytöstä.");
+                        break;
+                    default:
+                        System.out.println("Syöte oli väärä");
+                }
+            }
+    sc.close();    
     }
 }
---
+++ src/main/java/viikko2tehtavat/Hedgehog.java
@@ -0,0 +1,11 @@
+package viikko2tehtavat;
+
+public class Hedgehog {
+    public String name = "Pikseli";
+    public int age = 5;
+
+    public Hedgehog(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+}
