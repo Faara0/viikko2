de2cc394800b73e82b074e928ca060d9c927bf5d
Faara0 <toivovanhasaari@gmail.com>
2024-01-22 19:42:02 +0200

    siili

--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -1,39 +1,48 @@
 package main;
 
 import java.util.Scanner;
+
 public class App 
 {
-    public static void main( String[] args) {
-        int i = 0; //user gives input which action they want
-        do {
+    public static void main( String[] args){
+        Scanner sc = new Scanner(System.in);
+        boolean exit = false;
+        while(!exit) {
             System.out.println("1) Pistä siili puhumaan, 2) Luo uusi siili, 3) Juoksuta siiliä, 0) Lopeta ohjelma");
-                Scanner bufferedScanner = new Scanner(System.in);
-                String input = bufferedScanner.nextLine();
-                i = Integer.parseInt(input);
-                    if(i == 1) {
+    
+            if(sc.hasNext()) {
+                int i = 0;
+                String stringInput = sc.nextLine();
+                i = Integer.parseInt(stringInput);
+            
+                switch(i) {
+                    case 1:
                         System.out.println("Mitä siili sanoo: ");
-                            Scanner SpeakScanner = new Scanner(System.in);
-                            String speak = SpeakScanner.nextLine();
-                            Hedgehog siili = new Hedgehog();
-                            String speaking = siili.speak(speak);
-                            System.out.println(speaking);
-                    }
-                    else if(i == 2) {
-                        System.out.println("Valinta 2 toimii.");
-                        //break;
-                    }
-                    else if(i == 3) {
-                        System.out.println("Valinta 3 toimii.");
-                        //break;
-                    }
-                    else if(i == 0) {
-                        System.out.println("Kiitos ohjelman käytöstä.");
+                        Scanner SpeakScanner = new Scanner(System.in);
+                        String speak = SpeakScanner.nextLine();
+                        Hedgehog siili = new Hedgehog();
+                        String speaking = siili.speak(speak);
+                        System.out.println(speaking);
+                        SpeakScanner.close();
                         break;
-                    }
-                    else {
+                   case 2:
+                       System.out.println("Valinta 2 toimii.");
+                       break;
+                    case 3:
+                        System.out.println("Valinta 3 toimii.");
+                        break;
+                    case 0:
+                        System.out.println("Kiitos ohjelman käytöstä.");
+                        exit = true;
+                        break;
+                    default:
                         System.out.println("Syöte oli väärä");
-                    }    
-        } while(i > 0);     
-    }
+                        break;
 
+                }
+            }
+    
+        }
+    sc.close();
+    }
 }
